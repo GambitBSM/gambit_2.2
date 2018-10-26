8e7ce6615608c1ca3ce091fcb37108d56f75e92c
José Eliel Camargo-Molina <>
2018-10-26 15:12:10 +0100

    Revert "Slight change to Vevacious inputs to include newest parameter manager compatible with SARAH automatic model generation."
    This reverts commit 006c30a994ac3043833ab7bda70ff5b7e6010c62.

--- SpecBit/src/SpecBit_MSSM_VS.cpp
+++ SpecBit/src/SpecBit_MSSM_VS.cpp
@@ -77,7 +77,7 @@ namespace Gambit
                 "/InitializationFiles/DefaultObjectInitializationFiles/PotentialFunctionInitialization.xml";
 
                 std::string ScaleAndBlockFile = vevaciouspath +
-                "ModelFiles/LagrangianParameters/MssmCompatibleWithSARAHManager.xml";
+                "ModelFiles/LagrangianParameters/MssmCompatibleWithSlhaOneAndSlhaTwoAndSarahOutputs.xml";
 
                 std::string PotentialFunctionClassType = runOptions.getValueOrDef<std::string>(
                 "FixedScaleOneLoopPotential", "potential_type");
@@ -90,7 +90,7 @@ namespace Gambit
                     "<VevaciousPlusPlusPotentialFunctionInitialization>\n"
                     " <LagrangianParameterManagerClass>\n"
                     "    <ClassType>\n"
-                    "      SARAHManager\n"
+                    "      SlhaCompatibleWithSarahManager\n"
                     "    </ClassType>\n"
                     "    <ConstructorArguments>\n"
                     "      <ScaleAndBlockFile>\n"
