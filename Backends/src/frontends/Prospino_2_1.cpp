515f63f13412ced23412ae9b5698c4c70c860f7b
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2019-04-10 16:59:53 +0100

    Extended the list of arguments passed to Prospino
    	modified:   Backends/include/gambit/Backends/frontends/Prospino_2_1.hpp
    	modified:   Backends/src/frontends/Prospino_2_1.cpp

--- Backends/src/frontends/Prospino_2_1.cpp
+++ Backends/src/frontends/Prospino_2_1.cpp
@@ -34,12 +34,91 @@ BE_NAMESPACE
 
     std::cout << "DEBUG: run_prospino: Begin..." << std::endl;
 
-    Finteger inlo = 0;
-    Finteger isq_ng_in = 1;
-    Finteger icoll_in = 2;
+    Finteger inlo = 1;                 // specify LO only[0] or complete NLO (slower)[1]
+    Finteger isq_ng_in = 1;            // specify degenerate [0] or free [1] squark masses
+    Finteger icoll_in = 1;             // collider : tevatron[0], lhc[1]
+    Fdouble energy_in = 13000.0;       // collider energy in GeV
+    Finteger i_error_in = 0;           // with central scale [0] or scale variation [1]
+
+    Fstring<2> final_state_in = "nn";  // select process
+    Finteger ipart1_in = 1;            //
+    Finteger ipart2_in = 2;            //
+    Finteger isquark1_in = 0;          //
+    Finteger isquark2_in = 0;          //
+
+    /* 
+      (From the Prospino code documentation)
+
+      Options for final_state_in:
+
+      ng     neutralino/chargino + gluino  
+      ns     neutralino/chargino + squark  
+      nn     neutralino/chargino pair combinations  
+      ll     slepton pair combinations  
+      sb     squark-antisquark  
+      ss     squark-squark  
+      tb     stop-antistop  
+      bb     sbottom-antisbottom  
+      gg     gluino pair  
+      sg     squark + gluino  
+      lq     leptoquark pairs (using stop1 mass) 
+      le     leptoquark plus lepton (using stop1 mass) 
+      hh     charged Higgs pairs (private code only!)
+      ht     charged Higgs with top (private code only!)
+
+      Squark and antisquark added, but taking into account different sb or ss.
+
+
+      Options for ipart1_in, ipart2_in:
+
+      final_state_in = ng,ns,nn
+      ipart1_in   = 1,2,3,4  neutralinos
+                    5,6      positive charge charginos
+                    7,8      negative charge charginos
+      ipart2_in the same
+          chargino+ and chargino- different processes
+                                                                               
+      final_state_in = ll
+      ipart1_in   = 0        sel,sel + ser,ser  (first generation)
+                    1        sel,sel
+                    2        ser,ser
+                    3        snel,snel
+                    4        sel+,snl
+                    5        sel-,snl
+                    6        stau1,stau1
+                    7        stau2,stau2
+                    8        stau1,stau2
+                    9        sntau,sntau
+                   10        stau1+,sntau
+                   11        stau1-,sntau
+                   12        stau2+,sntau
+                   13        stau2-,sntau
+                   14        H+,H- in Drell-Yan channel
+                                                                               
+      final_state_in = tb and bb
+      ipart1_in   = 1        stop1/sbottom1 pairs
+                    2        stop2/sbottom2 pairs
+                                                                               
+      Note: Otherwise ipart1_in,ipart2_in have to set to one if not used.
+
+
+      Options for isquark1_in, isquark1_in:
+
+      for LO with light-squark flavor in the final state
+      isquark1_in     =  -5,-4,-3,-2,-1,+1,+2,+3,+4,+5
+                        (bL cL sL dL uL uR dR sR cR bR) in CteQ ordering
+      isquark1_in     = 0 sum over light-flavor squarks throughout
+                          (the squark mass in the data files is then averaged)
+
+      flavors in initial state: only light-flavor partons, no bottoms
+                                bottom partons only for Higgs channels
+
+      flavors in final state: light-flavor quarks summed over five flavors
+    */
+
 
     // Call prospino
-    prospino_gb(inlo, isq_ng_in, icoll_in);
+    prospino_gb(inlo, isq_ng_in, icoll_in, energy_in, i_error_in, final_state_in, ipart1_in, ipart2_in, isquark1_in, isquark2_in);
 
     std::cout << "DEBUG: run_prospino: ...End" << std::endl;
 
