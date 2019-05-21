5778bb2c3e8684ec71a94d049196f1582b6bf8bf
Laen111 <nealak@rogers.com.com>
2019-05-21 16:00:50 -0400

    map_to_cap set for mwimp, jwimp, and sigmav, testing

--- Models/include/gambit/Models/models/NREO.hpp
+++ Models/include/gambit/Models/models/NREO.hpp
@@ -19,15 +19,16 @@
 
 #define MODEL NREO
 	START_MODEL
-	DEFINEPARS(j, m)
+	DEFINEPARS(j, m, sig_v)
 	DEFINEPARS(c0_1, c0_2, c0_3, c0_4, c0_5, c0_6, c0_7, c0_8, c0_9, c0_10, c0_11, c0_12, c0_13, c0_14, c0_15) 
 	DEFINEPARS(c1_1, c1_2, c1_3, c1_4, c1_5, c1_6, c1_7, c1_8, c1_9, c1_10, c1_11, c1_12, c1_13, c1_14, c1_15)
 
 	/// To declare this kind of relationship between a parameter 'my_par' and a 
 	/// capability 'capability', one adds the following to the declaration of the 
 	/// model containing 'my_par': MAP_TO_CAPABILITY(my_par, capability)
-	MAP_TO_CAPABILITY(m, mwimpNREO)
-	MAP_TO_CAPABILITY(j, jwimpNREO)
+	MAP_TO_CAPABILITY(m, mwimp) /// this capability already exists in DarkBit
+	MAP_TO_CAPABILITY(j, jwimp) /// this is a new capability
+	MAP_TO_CAPABILITY(sig_v, sigmav) /// this capability already exists in DarkBit - not sure if I should actually be setting it, testing
 
 	MAP_TO_CAPABILITY(c0_1, c0_1_cap)
 	MAP_TO_CAPABILITY(c0_2, c0_2_cap)
