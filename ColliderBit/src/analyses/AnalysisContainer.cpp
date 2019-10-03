501d7f87946f0f2268e43f2a10220d29f3350468
Andy Buckley <a.g.buckley@gmail.com>
2019-10-03 20:49:07 +0100

    Declare new analysis

--- ColliderBit/src/analyses/AnalysisContainer.cpp
+++ ColliderBit/src/analyses/AnalysisContainer.cpp
@@ -57,6 +57,7 @@ namespace Gambit
       F(Covariance)                                  \
       F(ATLAS_13TeV_0LEP_13invfb)                    \
       F(ATLAS_13TeV_0LEP_36invfb)                    \
+      F(ATLAS_13TeV_0LEP_139invfb)                   \
       F(ATLAS_13TeV_0LEPStop_36invfb)                \
       F(ATLAS_13TeV_2LEPStop_36invfb)                \
       F(ATLAS_13TeV_MultiLEP_confnote_36invfb)       \
