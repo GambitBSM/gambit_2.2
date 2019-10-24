3d5cac5f809aa7a9532ecd63712a36ffe3fbd60d
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2019-10-24 18:24:00 +1100

    FS now loads fine after patching the make system

--- Backends/patches/flexiblesusy/2.0.1/CMSSM/make_flexiblesusy_CMSSM_patch.sh
+++ Backends/patches/flexiblesusy/2.0.1/CMSSM/make_flexiblesusy_CMSSM_patch.sh
@@ -3,6 +3,8 @@ cd ../../../../downloaded/
 tar -xvf FlexibleSUSY-2.0.1.tar.gz
 mkdir FlexibleSUSY-2.0.1/models/CMSSM
 cp -r ../../Models/data/FlexibleSUSY/2.0.1/CMSSM/* ./FlexibleSUSY-2.0.1/models/CMSSM/
+echo "diff -rupN FlexibleSUSY-2.0.1/models/CMSSM/module.mk ../installed/flexiblesusy/2.0.1/CMSSM/models/CMSSM/module.mk" >> patch_flexiblesusy_2.0.1_CMSSM.dif
+diff -rupN FlexibleSUSY-2.0.1/models/CMSSM/module.mk ../installed/flexiblesusy/2.0.1/CMSSM/models/CMSSM/module.mk >> patch_flexiblesusy_2.0.1_CMSSM.dif
 echo "diff -rupN FlexibleSUSY-2.0.1/models/CMSSM/CMSSM_slha_io.hpp ../installed/flexiblesusy/2.0.1/CMSSM/models/CMSSM/CMSSM_slha_io.hpp" >> patch_flexiblesusy_2.0.1_CMSSM.dif
 diff -rupN FlexibleSUSY-2.0.1/models/CMSSM/CMSSM_slha_io.hpp ../installed/flexiblesusy/2.0.1/CMSSM/models/CMSSM/CMSSM_slha_io.hpp >> patch_flexiblesusy_2.0.1_CMSSM.dif
 echo "diff -rupN FlexibleSUSY-2.0.1/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp ../installed/flexiblesusy/2.0.1/CMSSM/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp" >> patch_flexiblesusy_2.0.1_CMSSM.dif
--- Backends/patches/flexiblesusy/2.4.0/CMSSM/make_flexiblesusy_CMSSM_patch.sh
+++ Backends/patches/flexiblesusy/2.4.0/CMSSM/make_flexiblesusy_CMSSM_patch.sh
@@ -4,12 +4,12 @@ tar -xvf FlexibleSUSY-2.4.0.tar.gz
 mkdir FlexibleSUSY-2.4.0/models
 mkdir FlexibleSUSY-2.4.0/models/CMSSM
 cp -r ../../Models/data/FlexibleSUSY/2.4.0/CMSSM/* ./FlexibleSUSY-2.4.0/models/CMSSM/
+echo "diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/module.mk ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/module.mk" >> patch_flexiblesusy_2.4.0_CMSSM.dif
+diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/module.mk ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/module.mk >> patch_flexiblesusy_2.4.0_CMSSM.dif
 echo "diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_slha_io.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_slha_io.hpp" >> patch_flexiblesusy_2.4.0_CMSSM.dif
 diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_slha_io.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_slha_io.hpp >> patch_flexiblesusy_2.4.0_CMSSM.dif
 echo "diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp" >> patch_flexiblesusy_2.4.0_CMSSM.dif
 diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_two_scale_spectrum_generator.hpp >> patch_flexiblesusy_2.4.0_CMSSM.dif
-echo "diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_model_slha.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_model_slha.hpp" >> patch_flexiblesusy_2.4.0_CMSSM.dif
-diff -rupN FlexibleSUSY-2.4.0/models/CMSSM/CMSSM_model_slha.hpp ../installed/flexiblesusy/2.4.0/CMSSM/models/CMSSM/CMSSM_model_slha.hpp >> patch_flexiblesusy_2.4.0_CMSSM.dif
 mv patch_flexiblesusy_2.4.0_CMSSM.dif ../patches/flexiblesusy/2.4.0/CMSSM
 rm -r FlexibleSUSY-2.4.0
 cd ../patches/flexiblesusy/2.4.0/CMSSM
