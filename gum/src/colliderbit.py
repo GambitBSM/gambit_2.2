52d5a8eb090003928eae8cca7fc887f87de96293
Sanjay Bloor <sanjay.bloor12@imperial.ac.uk>
2019-11-11 16:59:25 +1000

    HiggsCouplingsTable switches for new model
    	modified:   gum
    	modified:   src/colliderbit.py

--- gum/src/colliderbit.py
+++ gum/src/colliderbit.py
@@ -8,6 +8,9 @@ from files import *
 
 
 def new_colliderbit_model(cb_output_dir, model):
+    """
+    Creates a new ColliderBit model from the templates generated previously.
+    """
 
     # Make sure that the output directories are in place
     dirs = [cb_output_dir + "/include/gambit/ColliderBit/models/", cb_output_dir + "/src/models/"]
@@ -22,3 +25,31 @@ def new_colliderbit_model(cb_output_dir, model):
                 newline = re.sub("@MODEL@", model, line)
                 newline = re.sub("@DATETIME@", datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"), newline)
                 f_new.write(newline)
+
+
+def new_hct_switch(model_name, spectrum, neutral_higgses):
+    """
+    Adds a new ModelInUse switch to the HiggsCouplingsTable
+    routines in ColliderBit/src/ColliderBit_Higgs.cpp.
+    """
+
+    # Get the names of all neutral Higgses
+    entry = []
+    for higgs in neutral_higgses:
+        entry.append("\""+pdg_to_particle(higgs, gambit_pdgs)+"\"")
+
+    # Sort the higgses in numerical order - with the neutral ones first
+    entry = sorted(entry, key=str.swapcase)
+    listhiggses = ','.join(entry)
+
+    towrite_src = (
+                "if (ModelInUse(\"{0}\"))\n"
+                "{{\n"
+                "spectrum_dependency = &Dep::{1};\n"
+                "Higgses = initvector<str>({2});\n"
+                "}}\n"
+    ).format(model_name, spectrum, higgses)
+
+    towrite_head = (
+                 "MODEL_CONDITIONAL_DEPENDENCY({0}, Spectrum, {1})"
+    ).format(spectrum, model_name)
