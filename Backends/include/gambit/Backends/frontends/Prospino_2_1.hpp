d9a4c807224247e107e612fa96353118155fa117
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2019-11-17 03:03:15 +0100

    Prospino backend: Used C binding to fix the symbol names in libprospino.so that we need to connect to.
    	modified:   Backends/include/gambit/Backends/frontends/Prospino_2_1.hpp
    	modified:   Backends/patches/prospino/2.1/patch_prospino_2.1.dif

--- Backends/include/gambit/Backends/frontends/Prospino_2_1.hpp
+++ Backends/include/gambit/Backends/frontends/Prospino_2_1.hpp
@@ -32,8 +32,8 @@ BE_ALLOW_MODELS(MSSM63atQ_mA,MSSM63atMGUT_mA)
 
 // PROSPINO_GB(inlo,isq_ng_in,icoll_in,energy_in,i_error_in,final_state_in,ipart1_in,ipart2_in,isquark1_in,isquark2_in)
 // __xx_prospino_subroutine_MOD_prospino_gb
-BE_FUNCTION(prospino_gb_init, void, (Fstring<500>&), "__xx_prospino_subroutine_MOD_prospino_gb_init", "prospino_gb_init")
-BE_FUNCTION(prospino_gb, void, (Farray<Fdouble,0,6>&, Finteger&, Finteger&, Finteger&, Fdouble&, Finteger&, Fstring<2>&, Finteger&, Finteger&, Finteger&, Finteger&, Farray<Fdouble,1,20>&, Farray<Fdouble,0,99>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,4,1,4>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&), "__xx_prospino_subroutine_MOD_prospino_gb", "prospino_gb")
+BE_FUNCTION(prospino_gb_init, void, (Fstring<500>&), "C_prospino_gb_init", "prospino_gb_init")
+BE_FUNCTION(prospino_gb, void, (Farray<Fdouble,0,6>&, Finteger&, Finteger&, Finteger&, Fdouble&, Finteger&, Fstring<2>&, Finteger&, Finteger&, Finteger&, Finteger&, Farray<Fdouble,1,20>&, Farray<Fdouble,0,99>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,4,1,4>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&, Farray<Fdouble,1,2,1,2>&), "C_prospino_gb", "prospino_gb")
 
 // Convenience functions (registration)
 // BE_CONV_FUNCTION(run_prospino, map_str_dbl, (const SLHAstruct&, prospino_settings&), "prospino_LHC_xsec")
