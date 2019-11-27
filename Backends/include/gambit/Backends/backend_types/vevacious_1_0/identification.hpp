9ebeee5824d2bcef03b6e7a1d32e3e03c56ad3f3
Sanjay Bloor <sanjay.bloor12@imperial.ac.uk>
2019-11-27 17:59:01 +1000

    Should fix issue with having to build vevacious before running GUM - thanks Janina
    	new file:   ../backend_types/vevacious_1_0/abstract_VevaciousPlusPlus.hpp
    	new file:   ../backend_types/vevacious_1_0/forward_decls_abstract_classes.hpp
    	new file:   ../backend_types/vevacious_1_0/forward_decls_wrapper_classes.hpp
    	new file:   ../backend_types/vevacious_1_0/identification.hpp
    	new file:   ../backend_types/vevacious_1_0/loaded_types.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/abstract_VevaciousPlusPlus.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/forward_decls_abstract_classes.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/forward_decls_wrapper_classes.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/identification.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/loaded_types.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/wrapper_VevaciousPlusPlus.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/wrapper_VevaciousPlusPlus_decl.hpp
    	new file:   ../backend_types/vevacious_1_0/vevacious_1_0/wrapper_VevaciousPlusPlus_def.hpp
    	new file:   ../backend_types/vevacious_1_0/wrapper_VevaciousPlusPlus.hpp
    	new file:   ../backend_types/vevacious_1_0/wrapper_VevaciousPlusPlus_decl.hpp
    	new file:   ../backend_types/vevacious_1_0/wrapper_VevaciousPlusPlus_def.hpp
    	new file:   vevacious_1_0.hpp

---
+++ Backends/include/gambit/Backends/backend_types/vevacious_1_0/identification.hpp
@@ -0,0 +1,11 @@
+// Identify backend and set macro flags
+
+#include "gambit/Utils/cats.hpp"
+
+#define BACKENDNAME vevacious
+#define BACKENDLANG CXX
+#define VERSION 1.0
+#define SAFE_VERSION 1_0
+
+#undef DO_CLASSLOADING
+#define DO_CLASSLOADING 1
---
+++ Backends/include/gambit/Backends/backend_types/vevacious_1_0/vevacious_1_0/wrapper_VevaciousPlusPlus.hpp
@@ -0,0 +1,9 @@
+#ifndef __wrapper_VevaciousPlusPlus_vevacious_1_0_hpp__
+#define __wrapper_VevaciousPlusPlus_vevacious_1_0_hpp__
+
+
+#include "wrapper_VevaciousPlusPlus_decl.hpp"
+#include "wrapper_VevaciousPlusPlus_def.hpp"
+
+
+#endif /* __wrapper_VevaciousPlusPlus_vevacious_1_0_hpp__ */
