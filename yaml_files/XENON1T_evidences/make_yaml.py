7b55a6c203f4d3689c96274fe3a3a46f31e4561a
Andrew Fowlie <andrew.j.fowlie@qq.com>
2020-06-24 14:27:36 +1200

    script to make all the yamls for varying the priors

--- yaml_files/XENON1T_evidences/make_yaml.py
+++ yaml_files/XENON1T_evidences/make_yaml.py
@@ -7,9 +7,10 @@ gives yaml with prior ranges for the axion couplings between 1e-10 and 1e-5
 """
 
 import yaml
+import numpy as np
 
 
-def make_yaml(template_name, a, b):
+def alter_couplings(template_name, a, b, suffix=None):
 
     with open(template_name) as f:
         original = yaml.load(f)
@@ -17,17 +18,52 @@ def make_yaml(template_name, a, b):
     params = ["gaee", "gan", "gagg"]
 
     for p in params:
-        original["Parameters"]["GeneralALP"][p]["range"] = [a, b]
+        original["Parameters"]["GeneralALP"][p]["range"] = [float(a), float(b)]
 
-    return yaml.dump(original, default_flow_style=False)
+    if suffix is not None:
+        original["KeyValues"]["default_output_path"] += suffix
 
-    
+    return original
+
+
+def alter_tritium(template_name, a, suffix=None):
+
+    with open(template_name) as f:
+        original = yaml.load(f)
+
+    original["Parameters"]["XENON1T_NuisanceParameters"]["x_3H"]["sigs"] = [float(a)]
+    if suffix is not None:
+        original["KeyValues"]["default_output_path"] += suffix
+
+    return original
+
+
 if __name__ == "__main__":
     
-    import sys
+    # Tritium yamls
 
-    template_name = sys.argv[1]
-    a = float(sys.argv[2])
-    b = float(sys.argv[3])
+    sigma = np.linspace(0., 5, 10)
 
-    print(make_yaml(template_name, a, b))
+    for i, s in enumerate(sigma):
+        d = alter_tritium("bkg_tritium.yaml", s, "_{}".format(i))
+        name = "bkg_tritium_{}.yaml".format(i)
+        with open(name, "w") as f:
+            yaml.dump(d, f, default_flow_style=False)
+
+        d = alter_tritium("signal_tritium.yaml", s, "_{}".format(i))
+        name = "signal_tritium_{}.yaml".format(i)
+        with open(name, "w") as f:
+            yaml.dump(d, f, default_flow_style=False)
+
+    # Signal coupling yamls
+
+    center = -12.5
+    width = np.linspace(0., 7.5, 10)
+
+    for i, w in enumerate(width):
+        a = center - w
+        b = center + w
+        d = alter_couplings("signal.yaml", a, b, "_{}".format(i))
+        name = "signal_{}.yaml".format(i)
+        with open(name, "w") as f:
+            yaml.dump(d, f, default_flow_style=False)
