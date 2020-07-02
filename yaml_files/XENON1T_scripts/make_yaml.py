de5dd49e434faeda4b26b521aff544c18b6e749e
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2020-07-02 13:00:20 +1000

    Moved make_yaml script

--- yaml_files/XENON1T_scripts/make_yaml.py
+++ yaml_files/XENON1T_scripts/make_yaml.py
@@ -8,6 +8,8 @@ gives you lots of yamls I want to run.
 
 import yaml
 import numpy as np
+import sys
+import os
 
 
 def alter_couplings(template_name, a, b, suffix=None):
@@ -15,7 +17,7 @@ def alter_couplings(template_name, a, b, suffix=None):
     with open(template_name) as f:
         original = yaml.load(f)
 
-    params = ["gaee", "gan", "gagg"]
+    params = ["gaee", "gaN", "gagg"]
 
     for p in params:
         original["Parameters"]["GeneralALP"][p]["range"] = [float(a), float(b)]
@@ -46,30 +48,77 @@ def alter_tritium(template_name, a, suffix=None):
 
 if __name__ == "__main__":
 
-    # Tritium yamls
+    if len(sys.argv) < 2:
+        print "Error! Missing parent file.\nUsage make_yaml.py <parent_file>\n"
+        exit()
 
-    sigma = np.linspace(0.5, 5, 10)
+    # Parent file
+    filename = sys.argv[1]
 
-    for i, s in enumerate(sigma):
-        d = alter_tritium("bkg_tritium.yaml", s, "_{}".format(i))
-        name = "bkg_tritium_{}.yaml".format(i)
-        with open(name, "w") as f:
-            yaml.dump(d, f, default_flow_style=False)
+    # What I am varying, width in ALP model or tritium sigma on XENON1T nuisance model
+    alp = False
+    tritium = False
+    if "alp" in filename and not "3h" in filename:
+      alp = True
+    elif "3h" in filename:
+      tritium = True
 
-        d = alter_tritium("signal_tritium.yaml", s, "_{}".format(i))
-        name = "signal_tritium_{}.yaml".format(i)
-        with open(name, "w") as f:
-            yaml.dump(d, f, default_flow_style=False)
+    # Pattern for daughter files
+    daughter = '.'.join(filename.split('.')[:-1])
+    extension = '.' + filename.split('.')[-1]
+
+    # Make all daughters in a separate directory
+    if not os.path.isdir(daughter):
+      os.mkdir(daughter)
+
+    # Create temp file with all !imports resolved
+    tempfile = filename + "_temp"
+    with open(filename) as fin, open(tempfile, 'w') as fout:
+        for line in fin:
+
+          if "!import" in line: # if there is a !include, resolve it
+             includefile = line.split('import')[-1].strip()
+             fout.write(line.split('!import')[:-1][0] + '\n')
+             with open(includefile) as finclude:
+                 for line2 in finclude:
+                     fout.write(line2)
+         
+          else: # otherwise just copy it
+              fout.write(line)
+
+    if tritium:
+
+        # Tritium yamls
+
+        sigma = np.linspace(0.5, 5, 20)
+
+        for i, s in enumerate(sigma):
+            suffix = "_{}".format(i)
+            d = alter_tritium(tempfile, s, suffix)
+
+            name = daughter + '/' + daughter + suffix + extension
+            with open(name, "w") as f:
+                yaml.dump(d, f, default_flow_style=False)
+
 
     # Signal coupling yamls
 
-    center = -12.5
-    width = np.linspace(0., 15., 10).tolist() + np.linspace(2., 6., 10).tolist()
+    elif alp:
 
-    for i, w in enumerate(width):
-        a = center - 0.5 * w
-        b = center + 0.5 * w
-        d = alter_couplings("signal.yaml", 10.**a, 10.**b, "_{}".format(i))
-        name = "signal_{}.yaml".format(i)
-        with open(name, "w") as f:
-            yaml.dump(d, f, default_flow_style=False)
+        center = -12.5
+        width = np.linspace(0., 15., 10).tolist() + np.linspace(2., 6., 10).tolist()
+
+        for i, w in enumerate(width):
+            a = center - 0.5 * w
+            b = center + 0.5 * w
+            suffix = "_{}".format(i)
+            d = alter_couplings(tempfile, 10.**a, 10.**b, suffix)
+
+            name = daughter + '/' + daughter + suffix + extension
+            with open(name, "w") as f:
+                yaml.dump(d, f, default_flow_style=False)
+
+    # Delete tempfile
+    os.remove(tempfile)
+
+    # Done!
