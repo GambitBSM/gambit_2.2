f533769f8f76c9ff64d62db5b8eb1f75b9dd39d9
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2020-07-08 22:39:42 +1000

    Fixed ascii2hdf5 file

--- yaml_files/XENON1T_scripts/ascii2hdf5.py
+++ yaml_files/XENON1T_scripts/ascii2hdf5.py
@@ -1,8 +1,8 @@
 
 import sys
 import os
-import numpy as np
 import h5py
+import numpy as np
 
 if len(sys.argv) < 3:
     print "Usage: python ascii2hdf5 <output_hdf5_file> <path_to_ascii_files_1> [<path_to_ascii_files_2> ...]"
@@ -13,7 +13,7 @@ asciipaths = []
 for i in range(2,len(sys.argv)):
     asciipaths.append(sys.argv[i])
 
-
+print "Reading data from files..."
 data = {}
 base = ''
 for path in asciipaths:
@@ -26,11 +26,12 @@ for path in asciipaths:
          and len(name.split('.')[-1].split('_')) <= 2
          and "live" not in name
          and "info" not in name
-         and "txt_txt" not in name])
+         and "txt_txt" not in name
+         and "hdf5" not in name])
     print "Number of files = ", n
 
     # Read all files
-    for i in range(0,n):
+    for i in range(0,1):
 
         # Filename
         datafile = path + '/' + base + '.txt_' + str(i)
@@ -56,16 +57,16 @@ for path in asciipaths:
                 linedata = line.strip().split()
                 for i in range(0,len(linedata)):
                     if datasets[i] in data.keys():
-                        np.append(data[datasets[i]],linedata[i])
+                        data[datasets[i]].append(linedata[i])
                     else:
-                        data[datasets[i]] = np.full(1,linedata[i])
+                        data[datasets[i]] = [linedata[i]]
 
     # Read posterior info file
     datafile = path + '/' + base + '.txt_0_txt_0'
     infofile = path + '/' + base + '.txt_0_txt_info_0'
 
     # If there is only one file sometimes has _0 and sometimes it does not
-    if n == 1 and not os.path.exists(datafile):
+    if not os.path.exists(datafile):
         datafile = path + '/' + base + '.txt_txt'
         infofile = path + '/' + base + '.txt_txt_info'
 
@@ -84,16 +85,17 @@ for path in asciipaths:
             linedata = line.strip().split()
             for i in range(0,len(linedata)):
                 if datasets[i] in data.keys():
-                    np.append(data[datasets[i]],linedata[i])
+                    data[datasets[i]].append(linedata[i])
                 else:
-                    data[datasets[i]] = np.full(1,linedata[i])
+                    data[datasets[i]] = [linedata[i]]
 
 # Write the hdf5 file
+print "Printing hdf5 file..."
 with h5py.File(hdf5file, 'w') as fhdf5:
     group = fhdf5.create_group(base)
 
     for key in data.keys():
-        group.create_dataset(key, data=data[key])
+        group.create_dataset(key, data=np.array(data[key]))
         group.create_dataset(key+"_isvalid", data=np.full(len(data[key]), True, dtype=bool))
 
 
