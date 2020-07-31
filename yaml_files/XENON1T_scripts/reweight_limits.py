1824f113bd1160cb51d4c2944b9ccecfb22bb769
Andrew Fowlie <andrew.j.fowlie@qq.com>
2020-07-31 13:38:39 +1200

    2d plot for vary solar alp prior

--- yaml_files/XENON1T_scripts/reweight_limits.py
+++ yaml_files/XENON1T_scripts/reweight_limits.py
@@ -32,16 +32,21 @@ if __name__ == "__main__":
     name = "/home/andrew/Desktop/v2/xe1t_alp/scanner_plugins/MultiNest/native-.txt"
     data = np.loadtxt(name)
 
-    center = -12.5
-    widths = np.linspace(0., 15., 15)
+    centers = np.linspace(-13.5, -9.5, 40)
+    widths = np.linspace(0., 17., 40)
 
     for width in widths:
-        a = center - 0.5 * width
-        b = center + 0.5 * width
-        ua, ub = unit_breadth(a, b)
-        where = np.logical_and.reduce((data[:, 2] < ub, data[:, 2] > ua,
-                                       data[:, 3] < ub, data[:, 3] > ua,
-                                       data[:, 4] < ub, data[:, 4] > ua))
-        weight = data[where, 0].sum()
-        log_evidence = ln_z.xe1t_alp[0] + np.log(norm_factor(a, b)) + np.log(weight)
-        print(width, log_evidence)
+        for center in centers:
+            a = center - 0.5 * max(0.1, width)
+            b = center + 0.5 * max(0.1, width)
+            ua, ub = unit_breadth(a, b)
+            where = np.logical_and.reduce((data[:, 2] < ub, data[:, 2] > ua,
+                                           data[:, 3] < ub, data[:, 3] > ua,
+                                           data[:, 4] < ub, data[:, 4] > ua))
+            weight = data[where, 0].sum()
+            log_evidence = ln_z.xe1t_alp[0] + np.log(norm_factor(a, b)) + np.log(weight)
+
+            if width < 1 and center < -11.5:
+                log_evidence = max(log_evidence, ln_z.xe1t[0])
+
+            print(center, width, log_evidence)
