a11705e2bdcc7ca47b2df4bcefd59ba5bf90c6b7
Pat Scott <pscott@synchron.com>
2022-05-03 15:07:40 +1000

    Reinstated lost HDF5_RETRIEVABLE_TYPES 	modified:   Printers/include/gambit/Printers/printers/hdf5types.hpp

--- Printers/include/gambit/Printers/printers/hdf5types.hpp
+++ Printers/include/gambit/Printers/printers/hdf5types.hpp
@@ -43,6 +43,11 @@
   (triplet<double>)                    \
   (map_intpair_dbl)                    \
 
+#define HDF5_RETRIEVABLE_TYPES \
+  HDF5_TYPES \
+  (MSSM_SLHAstruct) \
+  (SMslha_SLHAstruct)
+
 #define HDF5_BACKEND_TYPES             \
   (DM_nucleon_couplings)               \
   (Flav_KstarMuMu_obs)                 \
