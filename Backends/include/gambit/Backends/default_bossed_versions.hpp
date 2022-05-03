62c1a2bb83b560051e51361906c407d6fba3d456
Pat Scott <pscott@synchron.com>
2022-05-03 14:59:44 +1000

    Fixed missed conflict 	modified:   Backends/include/gambit/Backends/default_bossed_versions.hpp

--- Backends/include/gambit/Backends/default_bossed_versions.hpp
+++ Backends/include/gambit/Backends/default_bossed_versions.hpp
@@ -24,11 +24,7 @@
 
 #define  Default_gm2calc 1_3_0
 #define  Default_Pythia 8_212
-<<<<<<< HEAD
-#define  Default_Pythia_EM 8_212
 #define  Default_HepLike 1_1
-=======
 #define  Default_vevacious 1_0
->>>>>>> master
 
 // Defaults added by GUM (do not remove this comment).
