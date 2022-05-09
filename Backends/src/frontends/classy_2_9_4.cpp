298f82256babd3a2d2af52f9912ccce6fefc8e05
Patrick Stöcker <patrick.stoecker@rwth-aachen.de>
2022-05-09 03:22:07 +0200

    Fix bug in 'class_get_unlensed_cl' introduced through copy-paste in 84de2212
    	modified:       Backends/src/frontends/classy_2_6_3.cpp
    	modified:       Backends/src/frontends/classy_2_9_3.cpp
    	modified:       Backends/src/frontends/classy_2_9_4.cpp
    	modified:       Backends/src/frontends/classy_exo_2_7_2.cpp

--- Backends/src/frontends/classy_2_9_4.cpp
+++ Backends/src/frontends/classy_2_9_4.cpp
@@ -127,8 +127,8 @@
     // get the raw (unlensed) Cl.
     std::vector<double> class_get_unlensed_cl(std::string spectype)
     {
-      // Get dictionary containing all (lensed) Cl spectra
-      map_str_pyobj cl_dict = cosmo.attr("lensed_cl")().cast<map_str_pyobj>();
+      // Get dictionary containing the raw (unlensed) Cl spectra
+      map_str_pyobj cl_dict = cosmo.attr("raw_cl")().cast<map_str_pyobj>();
 
       // Get only the relevant Cl as np array and steal the pointer to its data.
       pybind11::object cl_array_obj = cl_dict[spectype];
